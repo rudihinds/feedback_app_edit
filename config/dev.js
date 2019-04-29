26c1d18c432f7e26d70ba19226e53ba50e860a32
Rudi Hinds <rudihinds@Rudis-MacBook-Air.local>
2019-04-29 11:25:49 +0100

    sendgrid not working update

---
+++ config/dev.js
@@ -0,0 +1,15 @@
+//dev.js, don't commit this
+
+module.exports = {
+
+	googleClientID: '421898486675-5rn4tafu4fj85n5i9o2nhb2rfbsnqee5.apps.googleusercontent.com',
+	googleClientSecret: 'L7WH-gYKm-fe6yjrvjFGxDtP',
+	mongoURI: 'mongodb+srv://rudi:XXMbgV7Wq3NRX7mL@cluster0-s0tzr.mongodb.net/test?retryWrites=true',
+	cookieKey: 'redacted',
+	stripePublishableKey: 'pk_test_oBbIYsor0MX1C0sQaNTkQPjU00TyWludUP',
+	stripeSecretKey: 'sk_test_HE8fjQ7rFrvd8EKvcWWaf3w900AYtYs1UL',
+	sendGridKey: "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"
+	
+
+};
+
